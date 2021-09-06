d0be6cba7615d7b599287b9fbe44c1196271903d
vikasj1977 <37282510+vikasj1977@users.noreply.github.com>
2021-09-06 14:42:50 -0400

    added the function to return the numeric value of a Roman Numeral string

--- RomanNumeralsAddition/RomanNumeralsAdd.cs
+++ RomanNumeralsAddition/RomanNumeralsAdd.cs
@@ -29,11 +29,38 @@ namespace RomanNumeralsAddition
 
             var intSum1 = 0;
 
+            for (var i = 0; i < strRoman.Length; i++)
+            {
+                if (hashRoman.ContainsKey(strRoman[i].ToString()))
+                {
+                    //1st condition checks if the current char is not the last char so that we dont get index out of range error 
+                    //while checking the 2nd condition
+                    //2nd condition is used to check if the next roman numeral is greater in value than the current roman numeral, 
+                    //in that case we subtract the value of the current roman numeral from the next roman numeral
+                    if (i < strRoman.Length - 1 && Convert.ToInt32(hashRoman[strRoman[i].ToString()]) < Convert.ToInt32(hashRoman[strRoman[i + 1].ToString()]))
+                    {
+                        intSum1 += Convert.ToInt32(hashRoman[strRoman[i + 1].ToString()]) - Convert.ToInt32(hashRoman[strRoman[i].ToString()]);
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        intSum1 += Convert.ToInt32(hashRoman[strRoman[i].ToString()]);
+                    }
+                }
+            }
+
             return intSum1;
         }
 
         static void Main(string[] args)
         {
+            var strRoman1 = "LX";
+            var strRoman2 = "DCCL";
+
+            int answer = ConvertRomanNumeralToNumber(strRoman1) + ConvertRomanNumeralToNumber(strRoman2);
+
+            Console.WriteLine("The sum of Roman numerals (" + strRoman1 + ") and (" + strRoman2 + ") is : " + answer);
+            Console.ReadLine();
         }
     }
 }
